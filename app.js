1df8557bd8e76d508c86c9a7b0efd413b440611e
Siavash Ashkiani <sashkiani@iteris.com>
2020-01-28 08:59:00 -0500

    Completed the generateHtmlDoc function + some minor code improvements

--- app.js
+++ app.js
@@ -5,17 +5,16 @@ const Intern = require("./lib/intern");
 const inquirer = require('inquirer');
 var fs = require("fs");
 
-
-
-
 let prompts = new Prompts();
-require('events').EventEmitter.defaultMaxListeners = 15;
+//Siavash 1/28/2020 Added the following code to suppress the MaxListenersExceeded warning. 
+//I assume that the warning eventually reappears if the number of team members grows but I tested it with up to 8 team members and worked fine.
+require('events').EventEmitter.defaultMaxListeners = 25;
 
 let team = [];
 function start() {
     console.log("Starting...");
     team = [];
-    const answers = AskTeamQuestions();
+    AskTeamQuestions();
 }
 
 function getManagerInfo() {
@@ -36,7 +35,6 @@ function getMemberInfo(role) {
             memberPrompts = prompts.intern;
             break;
         default:
-
     }
     return inquirer.prompt(memberPrompts);
 }
@@ -54,18 +52,15 @@ async function AskTeamMemberInfo() {
                     team.push(int);
                     break;
                 default:
-
             }
         });
     });
 }
+
 async function AskTeamQuestions() {
-
     getManagerInfo().then(managerInfo => {
-
         let manager = new Manager(managerInfo.name, parseInt(managerInfo.empId), "Manager", managerInfo.email, managerInfo.officeNumber);
         team.push(manager);
-
         inquirer.prompt([{
             type: "number",
             message: "How many people work for the manager in this project?",
@@ -75,7 +70,6 @@ async function AskTeamQuestions() {
                 await AskTeamMemberInfo();
             }
             generateHtmlDoc();
-
         });
     });
 }
@@ -83,16 +77,58 @@ async function AskTeamQuestions() {
 function generateHtmlDoc() {
     let htmlTeamData = fs.readFileSync("./templates/team.html");
     let htmlTeamText = htmlTeamData.toString();
-    console.log(team[0]);
-    let htmlManageData = fs.readFileSync("./templates/manager.html");
-    let htmlManagerText = htmlManageData.toString();
-    htmlManagerText = htmlManagerText.replace("--managerName--", team[0].name);
-    htmlManagerText = htmlManagerText.replace("--role--", team[0].getRole());
-    htmlManagerText = htmlManagerText.replace("--id--", team[0].id);
-    htmlManagerText = htmlManagerText.replace("--email--", team[0].email);
-    htmlManagerText = htmlManagerText.replace("--officeNumber--", team[0].officeNumber);
+    let members = [];
+    team.forEach(member => {
+        switch (member.getRole()) {
+            case "Manager":
+                let htmlManagerData = fs.readFileSync("./templates/manager.html");
+                let htmlManagerText = htmlManagerData.toString();
+                htmlManagerText = htmlManagerText.replace("--managerName--", member.name);
+                htmlManagerText = htmlManagerText.replace("--role--", member.getRole());
+                htmlManagerText = htmlManagerText.replace("--id--", member.id);
+                htmlManagerText = htmlManagerText.replace("--email--", member.email);
+                htmlManagerText = htmlManagerText.replace("--officeNumber--", member.officeNumber);
+                htmlTeamText = htmlTeamText.replace("--Manager--", htmlManagerText);
+                break;
+            case "Engineer":
+                let htmlEngineerData = fs.readFileSync("./templates/engineer.html");
+                let htmlEngineerText = htmlEngineerData.toString();
+                htmlEngineerText = htmlEngineerText.replace("--engineerName--", member.name);
+                htmlEngineerText = htmlEngineerText.replace("--role--", member.getRole());
+                htmlEngineerText = htmlEngineerText.replace("--id--", member.id);
+                htmlEngineerText = htmlEngineerText.replace("--email--", member.email);
+                htmlEngineerText = htmlEngineerText.replace("--gitHub--", member.gitHub);
+                members.push(htmlEngineerText);
+                break;
+            case "Intern":
+                let htmlInternData = fs.readFileSync("./templates/intern.html");
+                let htmlInternText = htmlInternData.toString();
+                htmlInternText = htmlInternText.replace("--internName--", member.name);
+                htmlInternText = htmlInternText.replace("--role--", member.getRole());
+                htmlInternText = htmlInternText.replace("--id--", member.id);
+                htmlInternText = htmlInternText.replace("--email--", member.email);
+                htmlInternText = htmlInternText.replace("--university--", member.school);
+                members.push(htmlInternText);
+                break;
+            default:
+                break;
 
-    htmlTeamText = htmlTeamText.replace("--Manager--", htmlManagerText);
+        }
+    });
+
+    //Siavash 1/28/2020 Added the following loop to break the array into smaller chunks. In that way I can control number of rows and columns.
+    let membersString = "";
+    let i, j, chunk = 3;
+    for (i = 0, j = members.length; i < j; i += chunk) {
+        let temparray = members.slice(i, i + chunk);
+        membersString += '<div class="row">'
+        let cols = temparray.map(item => {
+            return '<div class="col-4">' + item + '</div>';
+        });
+        membersString += cols.join("");
+        membersString += '</div>'
+    }
+    htmlTeamText = htmlTeamText.replace("--Team--", membersString);
 
     fs.writeFileSync("./output/team.html", htmlTeamText);
 }
